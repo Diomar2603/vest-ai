7416882903b949c244acbdfa9b6b3566c3876b74
Victor Cobo <v.cobof@gmail.com>
2025-04-27 18:26:08 -0300

    trocando regra do lint de erro pra warning

--- vest-ai-frontend/eslint.config.mjs
+++ vest-ai-frontend/eslint.config.mjs
@@ -17,11 +17,9 @@ const eslintConfig = [
       "unused-imports": unusedImports,
     },
     rules: {
-      "unused-imports/no-unused-imports": "error",
-      "unused-imports/no-unused-vars": [
-        "warn",
-        { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
-      ],
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     },
   },
 ];
